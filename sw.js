b01a453a78452ba81c7388a1ab71607c62b9e8be
0d3 <34978339+0d3@users.noreply.github.com>
2025-11-17 23:14:34 +0700

    feat: Implement PWA caching and update manifest
    Adds a service worker for offline caching and updates the manifest.json file to include PWA configurations and icons. Removes unused build scripts from package.json.

---
+++ sw.js
@@ -0,0 +1,49 @@
+const CACHE_NAME = 'kitabah-cache-v2';
+const URLS_TO_CACHE = [
+  '/',
+  'index.html',
+  'index.css',
+  'index.tsx',
+  'manifest.json'
+];
+
+self.addEventListener('install', event => {
+  // Perform install steps
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        console.log('Opened cache');
+        return cache.addAll(URLS_TO_CACHE);
+      })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+        // If not in cache, fetch from network. This is a fallback for any unexpected requests.
+        return fetch(event.request);
+      }
+    )
+  );
+});
+
+self.addEventListener('activate', event => {
+  var cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.map(function(cacheName) {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
